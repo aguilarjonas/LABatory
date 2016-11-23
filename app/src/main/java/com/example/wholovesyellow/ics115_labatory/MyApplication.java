6c9ea536c8a2c7ee31104e3186b6bade343ded54
Vince Urag <vinceuragvfx@gmail.com>
2016-11-23 23:18:42 +0800

    added push notif

---
+++ app/src/main/java/com/example/wholovesyellow/ics115_labatory/MyApplication.java
@@ -0,0 +1,31 @@
+package com.example.wholovesyellow.ics115_labatory;
+
+import android.app.Application;
+import android.content.res.Configuration;
+
+import com.onesignal.OneSignal;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OneSignal.startInit(this).init();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+}
