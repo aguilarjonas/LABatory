55a2984d389068f6b717c290a4d651f422f08d81
Vince Urag <vinceuragvfx@gmail.com>
2016-11-25 21:40:47 +0800

    added swipetoreferesh on admin_req

--- app/src/main/java/com/example/wholovesyellow/ics115_labatory/RequestsFragment.java
+++ app/src/main/java/com/example/wholovesyellow/ics115_labatory/RequestsFragment.java
@@ -29,7 +29,8 @@ import cz.msebera.android.httpclient.Header;
 public class RequestsFragment extends Fragment {
 
     ArrayList<String> list = new ArrayList<String>();
-//    SwipeRefreshLayout swipeRefreshLayout;
+    ListView listView;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     public RequestsFragment() {
         // Required empty public constructor
@@ -39,16 +40,9 @@ public class RequestsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        ViewGroup vg = (ViewGroup) inflater.inflate(R.layout.fragment_admin_req, container, false);
+        final ViewGroup vg = (ViewGroup) inflater.inflate(R.layout.fragment_admin_req, container, false);
 
-//        swipeRefreshLayout = (SwipeRefreshLayout)vg.findViewById(R.id.swipe_container);
-//        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-//            @Override
-//            public void onRefresh() {
-//                refresh();
-//            }
-//        });
-
+        swipeRefreshLayout = (SwipeRefreshLayout)vg.findViewById(R.id.activity_main_swipe_refresh_layout);
         final ListView listView = (ListView) vg.findViewById(R.id.lv_admin_req);
         listView.setEmptyView(vg.findViewById(R.id.nothing_here));
 
@@ -86,18 +80,57 @@ public class RequestsFragment extends Fragment {
             }
         });
 
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                    refreshContent(vg);
+
+            }
+        });
+
 
 
         return vg;
     }
 
-//    private void refresh(){
-//        new Handler().postDelayed(new Runnable() {
-//            @Override public void run() {
-//                swipeRefreshLayout.setRefreshing(false);
-//            }
-//        }, 5000);
-//    }
+    private void refreshContent(ViewGroup vg){
+        final ListView listView = (ListView) vg.findViewById(R.id.lv_admin_req);
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.addHeader("Authorization", Model.getToken());
+
+        client.get("http://urag.co/labatory_api/api/requests", new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    ArrayList<String> list2 = new ArrayList<String>();
+                    String response = new String(responseBody, "UTF-8");
+                    JSONArray jArray = new JSONArray(response);
+
+                    for(int i=0; i<jArray.length(); i++){
+                        JSONObject json_data = jArray.getJSONObject(i);
+                        String request_from = json_data.getString("request_from");
+                        String request_item = json_data.getString("request_item");
+                        int request_id = json_data.getInt("request_id");
+                        int request_status = json_data.getInt("request_status");
+                        if(request_status == 0){
+                            list2.add("Request #" + request_id);
+                        }
+                    }
+                    ListViewItemsReqAdapter adapter = new ListViewItemsReqAdapter(getContext(), R.layout.fragment_admin_req, list2);
+                    listView.setAdapter(adapter);
+                    swipeRefreshLayout.setRefreshing(false);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                error.printStackTrace();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+        }
 
 
 }
